5dae06f431e4be94e2370a805d90dde312192276
Aylin Ambriz <>
2022-06-29 07:22:33 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,53 @@
+var followInsta = $('.followonInsta');
+//event caller
+followInsta.on("click",functionOne);
+
+function functionOne() {
+  window.open("https://www.instagram.com/stem__x/");
+}
+//above is for the instagram of stemX
+
+
+var applyWriter = $('.writer');
+applyWriter.on("click",functionTwo);
+
+function functionTwo() {  window.open("https://docs.google.com/forms/d/e/1FAIpQLSc6vfZ559d_5vlmDF22NYYB02P4ZlSEsFeIAO5pEJQmZ9ik6w/viewform?usp=send_form");
+}
+//above is for the writer button
+
+var applyEditor = $('.editor');
+applyEditor.on("click",functionThree);
+
+function functionThree() {  window.open("https://docs.google.com/forms/d/e/1FAIpQLSc6vfZ559d_5vlmDF22NYYB02P4ZlSEsFeIAO5pEJQmZ9ik6w/viewform");
+}
+
+var applysmManager =$('.sm');
+applysmManager.on("click",functionFour);
+function functionFour () {  window.open("https://docs.google.com/forms/d/e/1FAIpQLSdrn1rn1Fr-uE412A0RHbTVr2K-B0o_Ls2v-vgOYHP3ZAlYCw/viewform");
+}
+
+var linkk =$('.linktoDoc');
+linkk.on("click",functionFive);
+function functionFive () {  window.open("https://docs.google.com/document/d/1lAJpWJiwZJF4fJKVRpmReGgoShsiOS-f3pEBTYWt3HE/edit");
+}
+
+var applyyy =$('.buttonWont');
+applyyy.on("click",functionSix);
+function functionSix () {  window.open("https://docs.google.com/forms/d/e/1FAIpQLSf_TLpxE76kY2j0RAHfBKToNH8NPpCU0OOlkgm37nXRm3lQKg/formResponse");
+}
+
+var middle= $('.middleSchool');
+middle.on("click",functionSeven);
+function functionSeven () {
+  window.open("https://docs.google.com/forms/d/e/1FAIpQLScb54YckpG9aqB5bbRFy4L13lLlsabXQPUZgM0I00E9dGLCHw/viewform?usp=send_form");
+}
+
+var collegee= $('.college');
+collegee.on("click",functionEight);
+function functionEight () { window.open("https://docs.google.com/forms/d/e/1FAIpQLSf2L0ESO372NiDr1BM4FvyXFfMWIlL2gNmLsTJoANGqiJsNpw/viewform");
+}
+
+var prof= $('.professional');
+prof.on("click",functionNine);
+function functionNine () { window.open("https://docs.google.com/forms/d/e/1FAIpQLSdql7iXzqLMDE0oxaq3yDexzTKPO4J8wexp7dvSj7YbZqhFdA/viewform?usp=send_form");
+}
